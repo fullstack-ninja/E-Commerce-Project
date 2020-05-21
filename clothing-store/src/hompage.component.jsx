dd62993ed2ee6772b538da95f072e3143514cd4d
fullstack-ninja <42291759+fullstack-ninja@users.noreply.github.com>
2020-05-21 21:35:18 +0530

    inital setup

---
+++ clothing-store/src/hompage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="director-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Jackets</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
